e19d94a23eaab460b11047ec43a4f95edbd2576f
nhatvytran <nhatvy2356@gmail.com>
2018-10-24 15:53:28 +0700

    Add a static-pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,15 @@
+class StaticPagesController < ApplicationController
+  def home; end
+
+  def about; end
+
+  def contact; end
+
+  def courses; end
+
+  def news; end
+
+  def news_post; end
+
+  def teachers; end
+end
